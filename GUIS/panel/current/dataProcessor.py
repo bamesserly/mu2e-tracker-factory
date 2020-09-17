b86ecc360f6ac2ad839a2f85ba93422ca454f869
Ben Messerly <bamesserly@gmail.com>
2020-09-17 08:57:14 -0700

    Revert to v3.2.6.
    Move LPAL changes merged prematurely.

--- GUIS/panel/current/dataProcessor.py
+++ GUIS/panel/current/dataProcessor.py
@@ -1585,14 +1585,6 @@ class SQLDataProcessor(DataProcessor):
         self.callMethod(
             panel.recordPAAS, self.stripNumber(data[19]), None, "C"
         )  # PAAS C
-        if data[20] is not None:
-            self.callMethod(
-                self.procedure.loadFromLPAL, self.stripNumber(data[20]), "top"
-            )  # Upper LPAL
-        if data[21] is not None:
-            self.callMethod(
-                self.procedure.loadFromLPAL, self.stripNumber(data[21]), "bot"
-            )  # Lower LPAL
 
     # Straws
     def saveDataProcess2(self):
@@ -1603,25 +1595,31 @@ class SQLDataProcessor(DataProcessor):
 
         # Call all getters to store data in sql database
         self.callMethod(
-            self.procedure.recordEpoxyBatchLower, self.stripNumber(data[1])
+            self.procedure.loadFromLPAL, self.stripNumber(data[1]), "top"
+        )  # Upper LPAL
+        self.callMethod(
+            self.procedure.loadFromLPAL, self.stripNumber(data[2]), "bot"
+        )  # Lower LPAL
+        self.callMethod(
+            self.procedure.recordEpoxyBatchLower, self.stripNumber(data[3])
         )  # Lower Epoxy
         self.callMethod(
-            self.procedure.recordEpoxyTimeLower, *self.parseTimeTuple(data[2])
+            self.procedure.recordEpoxyTimeLower, *self.parseTimeTuple(data[4])
         )  # Lower Epoxy Time
         self.callMethod(
-            self.procedure.recordEpoxyBatchUpper, self.stripNumber(data[3])
+            self.procedure.recordEpoxyBatchUpper, self.stripNumber(data[5])
         )  # Upper Epoxy
         self.callMethod(
-            self.procedure.recordEpoxyTimeUpper, *self.parseTimeTuple(data[4])
+            self.procedure.recordEpoxyTimeUpper, *self.parseTimeTuple(data[6])
         )  # Upper Epoxy Time
-        self.callMethod(self.procedure.recordPaasAMaxTemp, data[5])  # PAAS-A Max Temp
-        self.callMethod(self.procedure.recordPaasBMaxTemp, data[6])  # PAAS-B Max Temp
+        self.callMethod(self.procedure.recordPaasAMaxTemp, data[7])  # PAAS-A Max Temp
+        self.callMethod(self.procedure.recordPaasBMaxTemp, data[8])  # PAAS-B Max Temp
         self.callMethod(
-            self.procedure.recordHeatTime, *self.parseTimeTuple(data[7])
+            self.procedure.recordHeatTime, *self.parseTimeTuple(data[9])
         )  # Heat Time
         self.callMethod(
-            panel.recordPAAS, self.stripNumber(data[8]), None, "B"
-        )  # PAAS B
+            panel.recordPAAS, self.stripNumber(data[10]), None, "B"
+        )  # PIR Right B
 
     # Wire Tensions
     def saveDataProcess3(self):
@@ -2297,8 +2295,6 @@ class SQLDataProcessor(DataProcessor):
             ),  # Working Time of Epoxy
             self.getBarcode(panel.getPAAS(L_R=None, letter="A")),  # PAAS A barcode
             self.getBarcode(panel.getPAAS(L_R=None, letter="C")),  # PAAS C barcode
-            self.getBarcode(self.procedure.getLPAL(top_bot="top")),  # Upper LPAL
-            self.getBarcode(self.procedure.getLPAL(top_bot="bot")),  # Lower LPAL
         ]
 
     # Straws
@@ -2306,6 +2302,8 @@ class SQLDataProcessor(DataProcessor):
         panel = self.panel()
         return [
             self.getBarcode(self.panel()),  # Panel
+            self.getBarcode(self.procedure.getLPAL(top_bot="top")),  # Upper LPAL
+            self.getBarcode(self.procedure.getLPAL(top_bot="bot")),  # Lower LPAL
             self.epoxyBarcode(self.procedure.getEpoxyBatchLower()),  # Lower Epoxy
             self.timeDelta(
                 self.procedure.getEpoxyTimeLower(),
