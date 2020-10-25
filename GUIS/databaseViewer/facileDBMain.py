48c98d2ae9346610f7b2bd0b12d8241e271658d3
adamarnett <adam.arnett1@gmail.com>
2020-10-25 01:00:28 -0500

    Added icon to upper left corner! - It's cute

--- GUIS/databaseViewer/facileDBMain.py
+++ GUIS/databaseViewer/facileDBMain.py
@@ -1250,7 +1250,6 @@ class facileDBGUI(QMainWindow):
 # fmt: on
 
 if __name__ == "__main__":
-    print(getpass.getuser())
     app = QApplication(sys.argv)  # make an app
     # app.setStyleSheet(qdarkstyle.load_stylesheet())    # darkmodebestmode
     window = facileDBGUI(Ui_MainWindow())  # make a window
