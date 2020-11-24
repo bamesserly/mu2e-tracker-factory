375dd59a5a16024bb7139102ca1862418c111b81
Ben Messerly <bamesserly@gmail.com>
2020-11-24 12:36:59 -0600

    Revert PANGUI.bat logging changes (to 209cf2f).
    Problem with the logging solution: while logging to a file,
    it doesn't simultaneously print to the cmd prompt.
    
    Additionally, I think it waits until the process is finished
    before it writes to the file. Ideally, it would write
    continuously.

--- GUIS/panel/current/PANGUI.bat
+++ GUIS/panel/current/PANGUI.bat
@@ -1,19 +1,4 @@
 @ECHO OFF
-
-REM Script that updates Desktop/Production with the latest PANGUI code
-
-REM=============================================================================
-REM Collect YYYMMDD-HHMMSS int a variable
-REM=============================================================================
-set CUR_YYYY=%date:~10,4%
-set CUR_MM=%date:~4,2%
-set CUR_DD=%date:~7,2%
-set CUR_HH=%time:~0,2%
-if %CUR_HH% lss 10 (set CUR_HH=0%time:~1,1%)
-set CUR_NN=%time:~3,2%
-set CUR_SS=%time:~6,2%
-set CUR_MS=%time:~9,2
-set SUBFILENAME=%CUR_YYYY%%CUR_MM%%CUR_DD%-%CUR_HH%%CUR_NN%%CUR_SS% 
-
+cls
 CD C:\Users\%USERNAME%\Desktop\Production\GUIS\panel\current
-python PANGUI.py > C:\Users\%USERNAME%\Desktop\MergeDownLogs\PANGUI_log_%SUBFILENAME%.txt 2>&1
+python PANGUI.py
