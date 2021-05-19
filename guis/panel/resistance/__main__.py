4cd7f552f67261c32874d0f16243028a4980e6f1
Ben Messerly <bamesserly@gmail.com>
2021-05-19 15:57:48 -0500

    Fix resist plotting script for project restruct.

--- guis/panel/resistance/__main__.py
+++ guis/panel/resistance/__main__.py
@@ -1,5 +1,10 @@
 import guis.panel.resistance.run_test.run_test as resistancegui
+import guis.panel.resistance.run_test.parse_and_graph as pag
+from sys import argv
 
 
 if __name__ == "__main__":
-    resistancegui.main()
+    if len(argv) > 1:
+        pag.parse_and_graph()
+    else:
+        resistancegui.main()
