eea853e392bb02953fea288be56b02a0aeb9a31d
Ben Messerly <bamesserly@gmail.com>
2021-06-11 15:49:55 -0500

    Simple Heater Monitor (#95)
    If heater python code crashes for any reason, we can now recover key numbers and timestamps from the arduino itself.
    
    The recovery program is called simple_monitor. The idea is: you show up Monday morning to find that the gui crashed before all the data was collected. And _before_ you unplug the arduino from the computer, you run this script, which will spit out all arduino output for a couple minutes. In that couple minutes it should spit out all of the important numbers (full list is below).
    
    Haven't tested that the temperatures and times actually fill correctly, but not unsafe to test in production, which I'm doing with 131p2 right now.
    
    Biggest changes are to the arduino code itself. I _think_ actually that old python code will work with new arduino code and visa versa.
    
    For both PAASes we have:
    tempA_max = -99; // max temp
    tempA_max_timestamp = 0; // time at which max temp was reached
    tempA_setpt_timestamp = 0; // time setpoint was reaches
    tempA_50_up_timestamp = 0; // time 50c was reached on the way up
    tempA_40_dn_timestamp = 0; // time 40c was reached on the way down
    
    * Initial commit of heater monitor.
    
    Idea is a standalone script that just reads the heater arduino after, say,
    pangui crashed, to extract key temperatures and timestamps.
    
    Began to modify arduino code, too.
    
    * Arduino saves several more temps and timestamps.
    
    Simple monitor does the right thing: observes, doesn't reset.
    
    Cleaned up simple_monitor code.
    
    PanelHeater python code: updated logger.

--- guis/panel/heater/PAAS_heater/PAAS_heater.ino
+++ guis/panel/heater/PAAS_heater/PAAS_heater.ino
@@ -24,6 +24,19 @@ float temp2;
 float initial_temp2;
 float usrsp;  // user choice setpoint temperature 
 
+// Save key metrics in case monitor crashes
+float tempA_max = -99; // max temp
+uint32_t tempA_max_timestamp = 0; // time at which max temp was reached
+uint32_t tempA_setpt_timestamp = 0; // time setpoint was reaches
+uint32_t tempA_50_up_timestamp = 0; // time 50c was reached on the way up
+uint32_t tempA_40_dn_timestamp = 0; // time 40c was reached on the way down
+
+float temp2_max = -99;
+uint32_t temp2_max_timestamp = 0;
+uint32_t temp2_setpt_timestamp = 0;
+uint32_t temp2_50_up_timestamp = 0;
+uint32_t temp2_40_dn_timestamp = 0;
+
 float setpoint2;  
 char paas2='x'; // placeholder for user choice of 2nd PAAS type
 const int32_t wait = 8; // time between data points [seconds]
@@ -32,134 +45,181 @@ int32_t holdstart;
 uint8_t state=0;
 
 void setup() {
-  Serial.begin(2000000);
-  // don't wait indefinitely for serial, which would prevent WDT reset
-  while (!Serial.available() && millis()<10000) {
-    delay(100);
-  }
-  FastPwmSetup();
-  maxamp.begin(MAX31865_2WIRE);
-  maxamp2.begin(MAX31865_2WIRE);
-  //wdt_enable(WDTO_2S); 
-  initial_temp2 = maxamp2.temperature(RNOMINAL_PTCO2, RREF);
+	Serial.begin(2000000);
+	// don't wait indefinitely for serial, which would prevent WDT reset
+	while (!Serial.available() && millis()<10000) {
+		delay(100);
+	}
+	FastPwmSetup();
+	maxamp.begin(MAX31865_2WIRE);
+	maxamp2.begin(MAX31865_2WIRE);
+	//wdt_enable(WDTO_2S); 
+	initial_temp2 = maxamp2.temperature(RNOMINAL_PTCO2, RREF);
 }
 
 void loop() {
-  // get updates from python interface if connected
-  if (Serial.available()){  
-    Serial.println("Enter second PAAS type (B or C) or enter 0 if heating PAAS-A only");
-    char usrkey[5];  // user choice of 2nd PAAS type and temperature setpoint
-    byte len = Serial.readBytesUntil('\n',usrkey,sizeof(usrkey));  // read bytes into usrkey until newline
-    // paas2=usrkey[0];
-    // usrsp=usrkey[1:];  ...
-    usrkey[len]=0; 
-    char *pKbd = usrkey; 
-    paas2 = *pKbd;  
-    pKbd++; 
-    usrsp = atof(pKbd); 
-    if (abs(usrsp-setpointA)>5){ // new setpoint from python interface either 34C or 55C / software timer fixed
-        state=0; // resets holdstart to get full holdtime at new setpoint
-        setpointA = min(usrsp,52); 
-        // setpointB based on PAAS-B correction for temperature difference at RTD location vs. bulk surface
-        if (setpointA>34){ setpointB=50; }
-        else { setpointB=33.9; }
-        // setpointC based on PAAS-C correction for temperature difference at RTD location vs. under baseplate
-        setpointC = min(setpointA,50); 
-    }
-  }
-  // control heat cycle
-  // with 2nd PAAS type set, heater control will run with or without connection to python interface
-  if (paas2=='b' | paas2=='c' | paas2=='0'){
-    if (paas2=='b') setpoint2=setpointB;
-    else setpoint2=setpointC; // heat cycle for PAAAS-A alone does not use setpoint2
-    //wdt_enable(WDTO_2S); // measurements and PWM values take about 332ms
-    static uint32_t start;  
-    uint32_t now = millis();  
-    int32_t hasBeen = now - start;
-    if (hasBeen>1000*wait){
-      display_status();
-      if (state==0){  // increase temperature
-        tempA = maxamp.temperature(RNOMINAL_PTCO, RREF);
-        if (tempA>setpointA){  // start hold phase
-          holdstart = millis();
-          state = 1;
-        }
-        temp_control();
-        start = now;
-      }
-      else{  // hold temperature at setpoint
-        if ((millis()-holdstart)/60000 < holdtime){
-          // control temperature as in state 0
-          display_status();
-          temp_control();
-          start = now;
-        }
-        else{
-          // shut off power
-          display_status();
-          Serial.println("Timer has shut off power");
-          FastPwm(0,0);
-          start = now;
-        }     
-      }
-    }
-    delay(10);
-    //wdt_reset();
-    delay(100); // does not wait indefinitely for serial availability
-  }
+	// get updates from python interface if connected
+	if (Serial.available()){  
+		Serial.println("Enter second PAAS type (B or C) or enter 0 if heating PAAS-A only");
+		char usrkey[5];  // user choice of 2nd PAAS type and temperature setpoint
+		byte len = Serial.readBytesUntil('\n',usrkey,sizeof(usrkey));  // read bytes into usrkey until newline
+		// paas2=usrkey[0];
+		// usrsp=usrkey[1:];  ...
+		usrkey[len]=0; 
+		char *pKbd = usrkey; 
+		paas2 = *pKbd;  
+		pKbd++; 
+		usrsp = atof(pKbd); 
+		if (abs(usrsp-setpointA)>5){ // new setpoint from python interface either 34C or 55C / software timer fixed
+			state=0; // resets holdstart to get full holdtime at new setpoint
+			setpointA = min(usrsp,52); 
+			// setpointB based on PAAS-B correction for temperature difference at RTD location vs. bulk surface
+			if (setpointA>34){ setpointB=50; }
+			else { setpointB=33.9; }
+			// setpointC based on PAAS-C correction for temperature difference at RTD location vs. under baseplate
+			setpointC = min(setpointA,50); 
+		}
+	}
+	// control heat cycle
+	// with 2nd PAAS type set, heater control will run with or without connection to python interface
+	if (paas2=='b' | paas2=='c' | paas2=='0'){
+		if (paas2=='b') setpoint2=setpointB;
+		else setpoint2=setpointC; // heat cycle for PAAAS-A alone does not use setpoint2
+		//wdt_enable(WDTO_2S); // measurements and PWM values take about 332ms
+		static uint32_t start;  
+		uint32_t now = millis();  
+		int32_t hasBeen = now - start;
+		if (hasBeen>1000*wait){
+			display_status();
+			if (state==0){  // increase temperature
+				tempA = maxamp.temperature(RNOMINAL_PTCO, RREF);
+				if (tempA>setpointA){  // start hold phase
+					holdstart = millis();
+					state = 1;
+				}
+				temp_control();
+				start = now;
+			}
+			else{  // hold temperature at setpoint
+				if ((millis()-holdstart)/60000 < holdtime){
+					// control temperature as in state 0
+					display_status();
+					temp_control();
+					start = now;
+				}
+				else{
+					// shut off power
+					display_status();
+					Serial.println("Timer has shut off power");
+					FastPwm(0,0);
+					start = now;
+				}     
+			}
+		}
+		delay(10);
+		//wdt_reset();
+		delay(100); // does not wait indefinitely for serial availability
+	}
 }
 
 void temp_control(){
-  tempA = maxamp.temperature(RNOMINAL_PTCO, RREF);
-  if (paas2!='0'){
-    float dT;
-    temp2 = maxamp2.temperature(RNOMINAL_PTCO2, RREF);
-    if (paas2=='c') {
-      // PAAS-C correction: RTD placed in corner measures lower temperature than under baseplate at 55C
-      dT = tempA - temp2 - 5.0*(tempA/setpointA);  // experimental
-    }
-    if (paas2=='b') {
-      // PAAS-B correction: RTD placed in corner measures lower temperature than bulk surface at 55C
-      dT = tempA - temp2 - 4.5*(max(0,temp2-34)/16);  // experimental: with cube foam
-      if (setpointB<34){ // try to get faster rise to 34C by altering where feedback constraint applies
-        // 0130. adding condition on temp2 to prevent heating if B not plugged in
-        if (tempA<31 && temp2>initial_temp2+2){dT=0;}
-        else {dT = tempA - temp2 - 5.0*(max(0,temp2-20)/14);}
-      }
-      // Heat to 55:
-      // Heat A and B at full blast unless TA>=46 && (TB<40 || TA>44), in
-      // which case use the default dT.
-      if (setpointA>50) {
-        if(tempA>=46 && (temp2<40 || temp2>44)){ ; }
-        else dT=0;
-      }
-    }
-    if (dT<0 && valA==255) valB+=int(round(Pb * dT)); // -> slow the heating of B
-    else if (dT>0 && valB==255) valA-=int(round(Pa * dT)); // -> slow the heating of A
-    else {
-      valA += int(round(Pa * (setpointA-tempA)));
-      valB += int(round(Pb * (setpoint2-temp2)));
-    }
-  }
-  else {  // PAAS-A only -> no temperature difference to consider
-    valA += int(round(Pa * (setpointA-tempA)));
-    valB =0;
-  }
-  valA = max(0,min(255,valA));
-  valB = max(0,min(255,valB));
-  Serial.print("valA: ");Serial.println(valA);
-  Serial.print("valB: ");Serial.println(valB);
-  FastPwm(valA,valB); 
+	tempA = maxamp.temperature(RNOMINAL_PTCO, RREF);
+	if (paas2!='0'){
+		float dT;
+		temp2 = maxamp2.temperature(RNOMINAL_PTCO2, RREF);
+		if (paas2=='c') {
+			// PAAS-C correction: RTD placed in corner measures lower temperature than under baseplate at 55C
+			dT = tempA - temp2 - 5.0*(tempA/setpointA);  // experimental
+		}
+		if (paas2=='b') {
+			// PAAS-B correction: RTD placed in corner measures lower temperature than bulk surface at 55C
+			dT = tempA - temp2 - 4.5*(max(0,temp2-34)/16);  // experimental: with cube foam
+			if (setpointB<34){ // try to get faster rise to 34C by altering where feedback constraint applies
+				// 0130. adding condition on temp2 to prevent heating if B not plugged in
+				if (tempA<31 && temp2>initial_temp2+2){dT=0;}
+				else {dT = tempA - temp2 - 5.0*(max(0,temp2-20)/14);}
+			}
+			// Heat to 55:
+			// Heat A and B at full blast unless TA>=46 && (TB<40 || TA>44), in
+			// which case use the default dT.
+			if (setpointA>50) {
+				if(tempA>=46 && (temp2<40 || temp2>44)){ ; }
+				else dT=0;
+			}
+		}
+		if (dT<0 && valA==255) valB+=int(round(Pb * dT)); // -> slow the heating of B
+		else if (dT>0 && valB==255) valA-=int(round(Pa * dT)); // -> slow the heating of A
+		else {
+			valA += int(round(Pa * (setpointA-tempA)));
+			valB += int(round(Pb * (setpoint2-temp2)));
+		}
+	}
+	else {  // PAAS-A only -> no temperature difference to consider
+		valA += int(round(Pa * (setpointA-tempA)));
+		valB =0;
+	}
+	valA = max(0,min(255,valA));
+	valB = max(0,min(255,valB));
+	Serial.print("valA: ");Serial.println(valA);
+	Serial.print("valB: ");Serial.println(valB);
+	FastPwm(valA,valB); 
+}
+
+void set_key_metrics(float temp_A, float temp_2, uint32_t now){
+	if(tempA_max < temp_A){ // A max temp
+		tempA_max_timestamp = now;
+		tempA_max = temp_A;
+	}
+	if(temp2_max < temp_2){ // 2 max temp
+		temp2_max_timestamp = now;
+		temp2_max = temp_2;
+	}
+	if(temp_A == setpointA){ // A set point time
+		tempA_setpt_timestamp = now;
+	}
+	if(temp_2 == setpoint2){ // 2 set point time
+		temp2_setpt_timestamp = now;
+	}
+	if(temp_A > 50 && tempA_setpt_timestamp <= 1){ // A rises to 50
+		tempA_50_up_timestamp  = now;
+	}
+	if(temp_2 > 50 && temp2_setpt_timestamp <= 1){ // 2 rises to 50
+		temp2_50_up_timestamp  = now;
+	}
+	if(temp_A < 40 && tempA_setpt_timestamp > 1 && tempA_40_dn_timestamp <= 1){ // A falls to 40
+		tempA_40_dn_timestamp  = now;
+	}
+	if(temp_2 < 40 && temp2_setpt_timestamp > 1 && temp2_40_dn_timestamp <= 1){ // 2 falls to 40
+		temp2_40_dn_timestamp  = now;
+	}
 }
 
 void display_status(){
-  //Serial.println("PAAS-B: RTD in corner -> expect lower temperature than surface");
-  //Serial.println("PAAS-C: testing calibration -> expect apparent temp. diff. up to 5C");
-  Serial.print("Temperature 1: "); Serial.println(maxamp.temperature(RNOMINAL_PTCO, RREF));
-  Serial.print("Temperature 2: "); Serial.println(maxamp2.temperature(RNOMINAL_PTCO2, RREF));
-  Serial.print("Time = ");Serial.println(millis());
-  //Serial.print("state = ");Serial.println(state); // test for software timer fix
-  delay(10);
+	//Serial.println("PAAS-B: RTD in corner -> expect lower temperature than surface");
+	//Serial.println("PAAS-C: testing calibration -> expect apparent temp. diff. up to 5C");
+	float temp_A = maxamp.temperature(RNOMINAL_PTCO, RREF);
+	float temp_2 = -99;
+	uint32_t now = millis();
+	if(paas2!='0'){
+		temp_2 = maxamp2.temperature(RNOMINAL_PTCO2, RREF);
+	}
+	set_key_metrics(temp_A, temp_2, now);
+	Serial.print("Temperature 1: "); Serial.println(temp_A);
+	Serial.print("Temperature 2: "); Serial.println(temp_2);
+	Serial.print("Time = ");Serial.println(millis());
+	//Serial.print("state = ");Serial.println(state); // test for software timer fix
+
+	Serial.print("Temp A max = "); Serial.println(tempA_max);
+	Serial.print("Temp A max timestamp = "); Serial.println(tempA_max_timestamp);
+	Serial.print("Temp A reach setpoint timestamp = "); Serial.println(tempA_setpt_timestamp);
+	Serial.print("Temp A rises to 50 timestamp = "); Serial.println(tempA_50_up_timestamp);
+	Serial.print("Temp A falls to 40 timestamp = "); Serial.println(tempA_40_dn_timestamp);
+
+	Serial.print("Temp 2 max = "); Serial.println(temp2_max);
+	Serial.print("Temp 2 max timestamp = "); Serial.println(temp2_max_timestamp);
+	Serial.print("Temp 2 reach setpoint timestamp = "); Serial.println(temp2_setpt_timestamp);
+	Serial.print("Temp 2 rises to 50 timestamp = "); Serial.println(temp2_50_up_timestamp);
+	Serial.print("Temp 2 falls to 40 timestamp = "); Serial.println(temp2_40_dn_timestamp);
+
+	delay(10);
 }
-
-
--- guis/panel/heater/simple_monitor.py
+++ guis/panel/heater/simple_monitor.py
@@ -1,23 +1,12 @@
-#### run_heat_control.py
-#### python interface to collect and visualize data from [PAAS_heater_1009.ino]
-#### variable temperature setpoint input
+#### Simply read heater box output, assuming it's currently running
 
 import serial  ## from pyserial
 import serial.tools.list_ports
 import time, csv, sys, os
 from datetime import datetime as dt
 import numpy as np
-import matplotlib.pyplot as plt
 import traceback
 import threading
-
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-
-from guis.panel.heater.heat_control_window import (
-    Ui_MainWindow,
-)  ## edit via heat_control_window.ui in Qt Designer
 
 import logging
 
@@ -25,20 +14,13 @@ logger = logging.getLogger("root")
 
 from guis.common.getresources import GetProjectPaths
 
-import matplotlib
 
-matplotlib.use("Qt5Agg")
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-
-
-class HeatControl(QMainWindow):
+class HeatControl:
     """Python interface to collect and visualize data from PAAS heater control system"""
 
     def __init__(
         self, port, panel, wait=120000, ndatapts=450, parent=None, saveMethod=None
     ):
-        super(HeatControl, self).__init__(parent)
         if port == "GUI":  # PANGUI doesn't have a get port function
             port = getport("VID:PID=2341:8037")  # opened in PANGUI w/ port = "GUI"
         self.port = port
@@ -46,7 +28,7 @@ class HeatControl(QMainWindow):
         self.wait = wait  # wait interval between data points (ms)
         self.ndatapts = ndatapts  # number data points to collect
         self.hct = None  # heater control data thread
-        self.interval = QTimer()  # timer for interval between data points
+        # self.interval = QTimer()  # timer for interval between data points
         self.saveMethod = saveMethod  # pass data to PANGUI
 
         ## records for realtime plot
@@ -54,20 +36,6 @@ class HeatControl(QMainWindow):
         self.tempArec = []  # PAAS-A temperature [C]
         self.temp2rec = []  # 2nd PAAS temperature [C]
 
-        ## set up GUI
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.start_button.clicked.connect(self.start_data)
-        self.testplot3()
-
-        ## 2nd PAAS type selection required before start of data collection
-        self.ui.paas2_box.currentIndexChanged.connect(self.selectpaas)
-        self.ui.setpt_box.currentIndexChanged.connect(self.selectpaas)
-        self.ui.start_button.setDisabled(True)
-        self.ui.end_data.clicked.connect(self.endtest)
-
-        ## user choice temperature setpoint
-        self.ui.setpt_box.currentIndexChanged.connect(self.update_setpoint)
         logger.info("initialized")
 
     def saveMethod_placeholder(self):
@@ -75,37 +43,20 @@ class HeatControl(QMainWindow):
         ## self.temp2rec = PAAS-B or PAAS-C temperatures
         print("last temperature measurements:", self.tempArec[-1], self.temp2rec[-1])
 
-    def selectpaas(self):
-        """Enable start data collection if both 2nd PAAS and setpoint selected"""
-        if (
-            self.ui.paas2_box.currentText() != "Select..."
-            and self.ui.setpt_box.currentText() != "Select..."
-            and self.ui.paas2_box.isEnabled()
-        ):
-            self.ui.start_button.setEnabled(True)
-        else:
-            self.ui.start_button.setDisabled(True)
-
     def update_setpoint(self):
         """Get initial user choice temperature setpoint, or change setpoint
         partway through heat cycle, e.g. after funnels removed"""
-        if self.ui.setpt_box.currentText() != "Select...":
-            self.setpt = int(self.ui.setpt_box.currentText())
-            self.ui.labelsp.setText(f"Current setpoint: {self.setpt}C")
-            # if thread already running, send it the new setpoint
-            if self.hct:
-                logger.info("sending sp %s" % self.setpt)
-                self.hct.setpt = self.setpt
-        else:  # for case with 'Select...' option (start will be disabled)
-            self.setpt = 0
-            self.ui.labelsp.setText(f"Current setpoint: {self.setpt}C")
+        self.setpt = int(55)
+        # if thread already running, send it the new setpoint
+        if self.hct:
+            logger.info("sending sp %s" % self.setpt)
+            self.hct.setpt = self.setpt
 
     def start_data(self):
-        """Start serial interface and data collection thread"""
-        self.ui.start_button.setDisabled(True)
-        self.ui.paas2_box.setDisabled(True)
-        self.ui.setpt_box.removeItem(0)  # cannot revert to 'Select...'
-        self.paas2input = self.ui.paas2_box.currentText().split()[0]
+        logger.info("HeatControl::start_data")
+        """ Start serial interface and data collection thread """
+        # paas2dict = {"PAAS-B": "b", "PAAS-C": "c", "None": "0"}
+        self.paas2input = "PAAS-B"
         logger.info("2nd PAAS type: %s" % self.paas2input)
         self.update_setpoint()  # initial temperature setpoint
         ## map combobox entries to character to send via serial
@@ -125,10 +76,11 @@ class HeatControl(QMainWindow):
         self.t0 = time.time()
 
         ## run data collection from separate thread to avoid freezing GUI
-        self.interval.timeout.connect(self.next)
+        # self.interval.timeout.connect(self.next) # every timeout calls the next function
         self.hct = DataThread(self.micro, self.panel, self.paastype, self.setpt)
         self.hct.start()
-        self.interval.start(self.wait)  # get data at every timeout
+        time.sleep(60)  # don't join (?) for 60 seconds
+        # self.interval.start(self.wait)  # timeout every wait seconds
 
     # In this function: pass temperatures out to PANGUI
     def next(self):
@@ -137,11 +89,11 @@ class HeatControl(QMainWindow):
         data_list = list(self.hct.transfer())
         if len(data_list) > 0:  # should only have one set of measurements
             ## display values in GUI, include values in record (for realtime plot)
-            self.ui.tempPA.setText(str(data_list[0][0]))
+            # self.ui.tempPA.setText(str(data_list[0][0]))
             self.tempArec.append(float(data_list[0][0]))
             if self.paas2input != "None":
                 self.temp2rec.append(float(data_list[0][1]))
-                self.ui.tempP2.setText(str(data_list[0][1]))
+                # self.ui.tempP2.setText(str(data_list[0][1]))
             else:  # PAAS-A only
                 self.temp2rec.append(0.0)  # (avoid -242 or 988)
             self.timerec.append((time.time() - self.t0) / 60.0)
@@ -150,35 +102,11 @@ class HeatControl(QMainWindow):
             else:
                 self.hct.savedata()
                 ## update plot display
-                self.testplot2()
 
             # Pass the data to the parent PANGUI
             # self.tempArec = PAAS-A temperatures
             # self.temp2rec = NULL or PAAS-B or PAAS-C temperature
             self.saveMethod(self.tempArec[-1], self.temp2rec[-1])
-
-    def testplot3(self):
-        """Set up canvas for plotting temperature vs. time"""
-        self.ui.data_widget = QWidget(self.ui.graphicsView)
-        layout = QHBoxLayout(self.ui.graphicsView)
-        self.z = np.array([[-10, -10]])
-        self.canvas = DataCanvas(
-            self.ui.data_widget,
-            data=self.z,
-            width=5,
-            height=4,
-            dpi=100,
-            xlabel="Time since start [minutes]",
-            ylabel="Temperature [C]",
-        )
-        layout.addWidget(self.canvas)
-        self.ui.data_widget.repaint()
-
-    def testplot2(self):
-        """Update plot: new [time,temperature] array"""
-        self.z = np.array([self.timerec, self.tempArec, self.temp2rec]).T
-        self.canvas.read_data(self.z, self.paas2input)
-        self.ui.data_widget.repaint()
 
     def closeEvent(self, event):
         """Prevent timer and thread from outliving main window, close serial"""
@@ -186,9 +114,10 @@ class HeatControl(QMainWindow):
 
     def endtest(self):
         """Join data collection thread to end or reset test"""
-        self.interval.stop()
+        # self.interval.stop()
         if self.hct:  ## must stop thread if it's running
-            self.hct.join(0.1)  ## make thread timeout
+            # self.hct.join(0.1)  ## make thread timeout
+            self.hct.join(10)  ## make thread timeout
             self.hct = None
 
 
@@ -196,6 +125,7 @@ class DataThread(threading.Thread):
     """Read data from Arduino in temperature control box"""
 
     def __init__(self, micro, panel, paastype, setpoint):
+        logger.info("DataThread::__init__")
         threading.Thread.__init__(self)
         self.running = threading.Event()
         self.running.set()
@@ -215,7 +145,10 @@ class DataThread(threading.Thread):
     def run(self):
         logger.info("thread running")
         n, nmax = 0, 40
+        # flag = self.running.wait(10)
+        # print(flag)
         while self.running.isSet():
+            logger.info("HERE")
             self.micro.write(self.paastype)
             self.micro.write(str(self.setpt).encode("utf8"))
             self.micro.write(b"\n")
@@ -229,13 +162,17 @@ class DataThread(threading.Thread):
                     self.micro.write(str(self.setpt).encode("utf8"))
                     self.micro.write(b"\n")
                     continue
-                # print(repr(test))
+                print(repr(test))  # print all output from arduino
                 if len(test.strip().split()) < 2:  # skip split line error
                     logger.info("skipping fragment of split line")
                     continue
                 if "val" in test:  # duty cycle 0-255 for voltage control
                     logger.info(test.strip())
-                elif "Temperature" in test:  # temperature reading
+                elif "Time" in test:  # time since start in milliseconds
+                    duration = float(test.strip().split()[-1])
+                    duration = round(duration / 3600000, 3)
+                    logger.info(f"Time since start {duration} hours")
+                elif "Temp" in test:  # temperature reading
                     test = test.strip().split()
                     try:
                         float(test[-1])
@@ -294,57 +231,6 @@ class DataThread(threading.Thread):
         logger.info("thread joined")
 
 
-class DataCanvas(FigureCanvas):
-    """Each canvas class will use this to embed a matplotlib in the PyQt5 GUI"""
-
-    def __init__(
-        self,
-        parent=None,
-        data=None,
-        width=2,
-        height=2,
-        dpi=100,
-        xlabel="xlabel",
-        ylabel="ylabel",
-    ):
-        self.fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = self.fig.add_subplot(111)
-        self.axes.clear()
-        FigureCanvas.__init__(self, self.fig)
-        self.setParent(parent)
-        FigureCanvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        self.axes.grid(True)
-        self.axes.set_ylabel(ylabel)
-        self.axes.set_xlabel(xlabel)
-        box = self.axes.get_position()
-        self.axes.set_position(
-            [
-                box.x0 + box.width * 0.05,
-                box.y0 + box.height * 0.05,
-                box.width * 1.0,
-                box.height * 1.0,
-            ]
-        )
-        self.prev = None
-        self.prev2 = None
-
-    def read_data(self, data, p2):
-        if p2 == "None":  # only PAAS-A
-            if self.prev:
-                self.prev.remove()
-            (self.prev,) = self.axes.plot(data[:, 0], data[:, 1], "g.")
-        else:  # plot temperature for 2 PAAS plates
-            if self.prev:
-                self.prev.remove()
-                self.prev2.remove()
-            (self.prev,) = self.axes.plot(data[:, 0], data[:, 1], "g.", label="PAAS-A")
-            (self.prev2,) = self.axes.plot(data[:, 0], data[:, 2], "b.", label=p2)
-            self.axes.legend()
-        self.fig.canvas.draw()
-        self.fig.canvas.flush_events()
-
-
 ######################################################################################
 
 
@@ -362,6 +248,9 @@ def getport(hardwareID):
 
 
 def run():
+    from guis.common.panguilogger import SetupPANGUILogger
+
+    logger = SetupPANGUILogger("root", "HeaterMonitor")
     # heater control uses Arduino Micro: hardware ID 'VID:PID=2341:8037'
     port = getport("VID:PID=2341:8037")
     logger.info("Arduino Micro at {}".format(port))
@@ -370,14 +259,9 @@ def run():
     sys.excepthook = traceback.print_exception
 
     # GUI
-    app = QApplication(sys.argv)
-    ctr = HeatControl(port, panel="MN000")
-    ctr.show()
-    app.exec_()
+    h = HeatControl(port, panel="MN000")
+    h.start_data()
 
 
 if __name__ == "__main__":
-    from guis.common.panguilogger import SetupPANGUILogger
-
-    logger = SetupPANGUILogger("root", "HeaterStandalone")
     run()
