4f62817da2167c069077e713b855121140a661df
adamarnett <adam.arnett1@gmail.com>
2021-06-18 16:04:59 -0500

    Resolved a conflict incorrectly I picked the wrong option while resolving a conflict, whoops.

--- guis/common/databaseClasses.py
+++ guis/common/databaseClasses.py
@@ -1950,16 +1950,13 @@ class Station(BASE, OBJECT):
         # print(PanelStation.query().first()) # None!
         # print(PanelStation.query().filter(PanelStation.production_step == day))
         try:
-            ret = PanelStation.query().filter(PanelStation.production_step == day).one()
-        except sqlalchemy.orm.exc.NoResultFound:
-            logger.error(f"No production stage/step not found for {day}.")
-            logger.error("This indicates a structural problem with the DB.")
-            exit()
-        except sqlalchemy.orm.exc.MultipleResultsFound:
-            logger.error(f"More than one production stage/step not found for {day}.")
-            logger.error("This indicates a structural problem with the DB.")
-            sys.exit()
-        return ret
+            return (
+                PanelStation.query()
+                .filter(PanelStation.production_step == day)
+                .one_or_none()
+            )
+        except sqlalchemy.exc.OperationalError:
+            logger.error("DB Locked. Panel station query failed.")
 
     def startSession(self):
         # Start new session and return
