27d0d7037da8b3beead2766da182f367eb548aaa
Ben Messerly <bamesserly@gmail.com>
2021-06-30 09:28:40 -0500

    Require meas from all three pros and require at least 50 data points for each pro

--- guis/panel/resistance/run_test/cleanup.py
+++ guis/panel/resistance/run_test/cleanup.py
@@ -43,7 +43,7 @@ def RenameColumns(df):
 
 
 # Make a df from a measurement csv file, f
-def MakeMeasurementDF(f,fname,panel_dir):
+def MakeMeasurementDF(f):
     df = pd.DataFrame()
 
     # read in csv, skip empty files
@@ -57,34 +57,35 @@ def MakeMeasurementDF(f,fname,panel_dir):
         # print(f"WARNING: {f.stem} produced empty df. Skipping this file.")
         return pd.DataFrame()
 
+    print(df.columns.str.strip())
+
     # some files have messed up headers
     # '# Position'   wire/straw   ADC values...   resistance   PASS?
     if "# Position" not in df.columns.str.strip():
         return pd.DataFrame()
 
-    # add panel, date, process info to df
-    df["panel"] = fname[1]
-    df["date"] = fname[2]
-    df["pro"] = fname[3]
-
     return df
 
 
-def ConsolidatePanelData(panel_dir):
+# combine all the data from list of files for panel into one df
+def ConsolidatePanelData(files, panel):
     dfs = []
-
-    for f in Path(panel_dir).rglob("*.csv"):
+    for f in files:
         df = pd.DataFrame()
         fname = f.stem.split("_")
 
         # new format filename ["resistance_test", "MN115", "20210628", "pro3"]
         if len(fname) == 4:
             # make sure this file belongs in this folder
-            if fname[1] == panel_dir.name:
-                df = MakeMeasurementDF(f, fname, panel_dir)
+            if fname[1] == panel:
+                df = MakeMeasurementDF(f)
+                # add panel, date, process info to df
+                df["panel"] = fname[1]
+                df["date"] = fname[2]
+                df["pro"] = fname[3]
             else:
                 print(
-                    f"WARNING: {f.stem} found in {panel_dir.name} dir.\n"
+                    f"WARNING: {f.stem} found in {panel} dir.\n"
                     f"Panel file and dir do not match. Skipping this file."
                 )
 
@@ -105,7 +106,7 @@ def ConsolidatePanelData(panel_dir):
             dfs.append(df)
 
     if dfs:
-        print(f"{panel_dir.name}: {len(dfs)} measurements found.")
+        print(f"{panel}: {len(dfs)} usable measurements found.")
         return pd.concat(dfs)
     else:
         return pd.DataFrame()
@@ -126,25 +127,35 @@ def ConsolidatePanelData(panel_dir):
 # * need to add the timestamp (from the filename) to a final column
 def IntermediateClean():
     topdir = GetProjectPaths()["datatop"] / "PanelResistance_2021-06-22_2" / "RawData"
+    panels = sorted(Path(topdir).glob("*"))
+    panel_data = []
     # Loop panels
-    for panel_dir in Path(topdir).glob("*"):
+    for panel_dir in panels:
         if not panel_dir.is_dir() or panel_dir.name.startswith("."):
             continue
 
-        if panel_dir.name  != "MN112":
-           continue
-        print(panel_dir.name)
-
-        # let's start by just looking at clean panels that have a single pro2, pro3, and QC measurements
-        # meas_parsed = [m.stem.split("_") for m in measurements]
-        # if (any("pro2" in i for i in meas_parsed) and
-        #    any("pro3" in i for i in meas_parsed) and
-        #    any("finalQC" in i for i in meas_parsed)):
-        #    print(f"    {panel_dir.name} has all three measurements.", len(measurements))
+        if panel_dir.name != "MN120":
+            continue
 
         # add all the measurements for this panel to single df
-        panel_df = ConsolidatePanelData(panel_dir)
+        files = sorted(Path(panel_dir).rglob("*.csv"))
+
+        # require panel to have pro2, pro3, and qc measurements
+        if (
+            any("proc2" in i.stem.split("_") for i in files)
+            and any("proc3" in i.stem.split("_") for i in files)
+            and any("finalQC" in i.stem.split("_") for i in files)
+        ):
+            pass
+            # print(f"    {panel_dir.name} has all three measurements.", len(files))
+        else:
+            # print(f"    {panel_dir.name} is missing a measurement.", len(files))
+            continue
+
+        # Collect all data into a single df
+        panel_df = ConsolidatePanelData(files, panel_dir.name)
         if panel_df.empty:
+            print(f"    empty df somehow for {panel_dir.name}.")
             continue
 
         # drop NaNs
@@ -153,12 +164,24 @@ def IntermediateClean():
         # column names in raw data are wrong
         panel_df = RenameColumns(panel_df)
 
+        # require 50 measurements for each process
+        n_pro2 = panel_df["pro"].astype(str).str.contains("proc2").sum()
+        n_pro3 = panel_df["pro"].astype(str).str.contains("proc3").sum()
+        n_qc = panel_df["pro"].astype(str).str.contains("finalQC").sum()
+        if n_pro2 < 50 or n_pro3 < 50 or n_qc < 50:
+            print("pro 2, 3, and qc datapoints:", n_pro2, n_pro3, n_qc)
+            print("one of these is < 50 so skipping this panel", panel_dir.name)
+            continue
+
+        panel_data.append(panel_df)
+
         print(panel_df.to_string())
+
+    return panel_data
 
 
 def main():
-    # ParseAll() # done
-    IntermediateClean()
-    # datafilename = parse_log(raw_data)
-    # print("Data file and plots are at", datafilename)
-    print("Hello world")
+    # ParseAll() # done, only needs to be done once
+    # list of data frames for the resistance measurement of each panel
+    panel_resistance_measurements = IntermediateClean()
+    print("Done")
