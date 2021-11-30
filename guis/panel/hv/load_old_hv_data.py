7bd8f245e7d32b7285123d8d4d668f5ab530ba02
Ben Messerly <bamesserly@gmail.com>
2021-11-30 10:42:06 -0600

    Submit dummy data point is working!
    Commented-out the csv-reading stuff. Got the db-reading/writing
    working.
    
    Tweaked Katie's SQL queries -- they don't need the ID.
    
    Next: read in real data from csv files!

--- guis/panel/hv/load_old_hv_data.py
+++ guis/panel/hv/load_old_hv_data.py
@@ -2,13 +2,33 @@
 # Load a hv data CSV file into the local database
 # ===============================================================================
 
+
+"""
+Reminder: these are the columns that we need to submit
+ procedure     INTEGER NOT NULL
+ position      INTEGER NOT NULL, (a number 0-96)
+ current_left  REAL,
+ current_right REAL, (right or left may be specified. If not, choose left).
+ voltage       REAL, (either 1500 or 1100)
+ is_tripped    BOOLEAN DEFAULT (either 0 (false) or 1 (true))
+ timestamp     INTEGER ("epoch" time (msec, sec), if no time avail, use file create datetime).
+
+step 1: submit a single data point with random values for the above columns
+"""
+
+
+import logging
+
+logger = logging.getLogger("root")
+
+
 import sqlalchemy as sqla  # for interacting with db
 import sys, csv
 import time
 from pathlib import Path
 from datetime import datetime as dt
 
-# from guis.common.getresources import GetProjectPaths, GetLocalDatabasePath
+from guis.common.getresources import GetProjectPaths, GetLocalDatabasePath
 
 kPROCESSES = list(range(1, 8))
 
@@ -37,40 +57,60 @@ def convert_to_epoch(date_time):
     return epoch
 
 
-def run(panel, process, data_file):
-    # database = Path("dummy.db")
-    try:
-        assert Path(data_file).is_file()
-    except AssertionError:
-        print(f"Data file {data_file} not found!")
-    # engine = sqla.create_engine("sqlite:///" + database)  # create engine
+def run(panel=100, process=3, data_file=None):
+    # try:
+    #    assert Path(data_file).is_file()
+    # except AssertionError:
+    #    print(f"Data file {data_file} not found!")
 
-    # with engine.connect() as connection:
-    #     pid = GetProcedureID(connection, int(panel), int(process))
+    database = GetLocalDatabasePath()
+    engine = sqla.create_engine("sqlite:///" + database)  # create engine
 
-    #     print(f"    Found PID for MN{panel} pro{process}: {pid}")
+    with engine.connect() as connection:
+        pid = GetProcedureID(connection, int(panel), int(process))
 
-    #     queryRight = """
-    #     INSERT OR IGNORE INTO measurement_pan5 (id, procedure, position, current_right, voltage, is_tripped, timestamp)
-    #     VALUES (?, ?, ?, ?, ?, ?, ?);
-    #     """
-    #     queryLeft = """
-    #     INSERT OR IGNORE INTO measurement_pan5 (id, procedure, position, current_left, voltage, is_tripped, timestamp)
-    #     VALUES (?, ?, ?, ?, ?, ?, ?);
-    #     """
+        print(f"    Found PID for MN{panel} pro{process}: {pid}")
 
-    with open(data_file, "r") as f:
-        dr = csv.DictReader(f)
-        # Entry of dr looks like
-        # OrderedDict([('Position', '68'), ('Current', '0.02'),
-        # ('Side', 'Right'), ('Voltage', '1500V'), ('IsTripped', '0'),
-        # ('Timestamp', '2021-04-13T14:07:16.219870')])
+        queryRight = """
+        INSERT OR IGNORE INTO measurement_pan5 (procedure, position, current_right, voltage, is_tripped, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?);
+        """
+        queryLeft = """
+        INSERT OR IGNORE INTO measurement_pan5 (procedure, position, current_left, voltage, is_tripped, timestamp)
+        VALUES (?, ?, ?, ?, ?, ?);
+        """
+        to_db = [(pid, 0, 0.5, 1500, 0, 123456)]
 
-        # to_db = [
-        #     (i["Position"], i["Current"], i["Voltage"], i["IsTripped"], i["Timestamp"])
-        #     for i in dr
-        # ]
+        try:
+            r_set = connection.execute(queryLeft, to_db)
+            print(r_set)
+        except sqla.exc.OperationalError as e:
+            logger.error(e)
+            error = str(e.__dict__["orig"])
+            logger.error(error)
+        except sqla.exc.IntegrityError as e:
+            logger.error(e)
+            error = str(e.__dict__["orig"])
+            logger.error(error)
+        else:
+            logger.info(f"    Loaded {r_set.rowcount} data points into local DB.")
+            logger.info(
+                "    To send it to the network (and see it in DBV) "
+                "trigger an automerge."
+            )
 
 
 if __name__ == "__main__":
     run(sys.argv[1], sys.argv[2], sys.argv[3])
+
+# with open(data_file, "r") as f:
+#    dr = csv.DictReader(f)
+#    # Entry of dr looks like
+#    # OrderedDict([('Position', '68'), ('Current', '0.02'),
+#    # ('Side', 'Right'), ('Voltage', '1500V'), ('IsTripped', '0'),
+#    # ('Timestamp', '2021-04-13T14:07:16.219870')])
+
+# to_db = [
+#    (i["Position"], i["Current"], i["Voltage"], i["IsTripped"], i["Timestamp"])
+#    for i in dr
+# ]
