0e8b2913979d2641fe9cf7a74db89608d987fac8
Ben Messerly <bamesserly@gmail.com>
2021-11-30 17:09:33 -0600

    Move general-use function into utilities file.
    Initial commit of loading script. Just a copy of the heat loading script atm.

--- guis/panel/leak/PlotLeakRate.py
+++ guis/panel/leak/PlotLeakRate.py
@@ -11,28 +11,21 @@
 # Next: pass the data to a function that loads the data into the DB.
 #
 ################################################################################
-import pandas as pd
 import matplotlib.pyplot as plt
 import optparse
 from scipy import interpolate, stats, signal
 import numpy as np
 from sklearn.linear_model import LinearRegression
-import datetime
 import os
 import re
 from guis.common.getresources import GetProjectPaths
 from pathlib import Path
 import sys
+from guis.panel.leak.panel_leak_utilities import *
 
 ################################################################################
 # Constants
 ################################################################################
-# Unit conversion factor for inches of water to psi.
-kINCHES_H2O_PER_PSI = 27.6799048
-
-# Unit conversion factor for PSI change per day to sccm
-kPSI_PER_DAY_TO_SCCM = 0.17995993587933934
-
 # Default relative time after which the fit begins (days)
 kFIT_START_TIME = 0.2
 
@@ -124,10 +117,6 @@ def GetOptions():
 ################################################################################
 # Helper functions
 ################################################################################
-def ConvertInchesH2OToPSI(pressure_inches_h2O):
-    return pressure_inches_h2O / kINCHES_H2O_PER_PSI
-
-
 # Raw data file name: "201109_1257_MN061test6.txt"
 def GetPanelIDFromFilename(filename):
     return re.search(r"MN\d\d\d", filename).group(0)
@@ -174,79 +163,6 @@ def PlotDataPoints(df, column_name, axis, color=None, label=None):
         markersize=markersize,
         label=label,
     )
-
-
-################################################################################
-# READ INPUT
-# Read the raw data (old or new format) into a dataframe
-################################################################################
-def ReadLeakRateFile(infile, is_new_format="true"):
-    if is_new_format:
-        # read input file
-        df = pd.read_csv(infile, engine="python", header=1, sep="\t")
-
-        # remove whitespace from column headers
-        df.columns = df.columns.str.replace(" ", "")
-
-        # convert pressure from inches of water to psi
-        df['Diff"H2O'] = ConvertInchesH2OToPSI(df['Diff"H2O'])
-        df = df.rename(columns={'Diff"H2O': "PRESSURE(PSI)"})
-
-        # rename time
-        df = df.rename(columns={"Elapdays": "TIME(DAYS)"})
-
-        # rename temps
-        df = df.rename(columns={"RoomdegC": "ROOM TEMPERATURE(C)"})
-        df = df.rename(columns={"EncldegC": "BOX TEMPERATURE(C)"})
-
-        ## make a new column: pressure/temp
-        # df['PSI/degC'] = df["PRESSURE(PSI)"]/df["RoomdegC"]
-
-        return df
-    else:
-        # read input file
-        df = pd.read_csv(infile, engine="python", header=4, sep=",", encoding="cp1252")
-
-        # remove whitespace from column headers
-        df.columns = df.columns.str.replace(" ", "")
-        df.columns = df.columns.str.replace("\t", "")
-        df.columns = df.columns.str.replace(u"°", "")
-
-        # convert pressure from inches of water to psi
-        df["PRESSURE(INH20D)"] = ConvertInchesH2OToPSI(df["PRESSURE(INH20D)"])
-        df = df.rename(columns={"PRESSURE(INH20D)": "PRESSURE(PSI)"})
-
-        # rename temp
-        df = df.rename(columns={"TEMPERATURE(C)": "BOX TEMPERATURE(C)"})
-
-        # convert absolute time to elapsed days
-        def get_time(time_str):
-            """Converts a time string from the datafile to UNIX time."""
-            if "." in time_str:
-                # More than 1 day has passed
-                days, time_s = time_str.split(".")
-            else:
-                # Less than 1 day has passed
-                days = "0"
-                time_s = time_str
-            days = int(days)
-            hours, minutes, seconds = map(int, time_s.split(":"))
-
-            td = datetime.timedelta(
-                days=days, hours=hours, minutes=minutes, seconds=seconds
-            )
-            total_seconds = int(round(td.total_seconds()))
-
-            return total_seconds / 24 / 60 / 60
-
-        df["TIME(hh:mm:ss)"] = df["TIME(hh:mm:ss)"].apply(get_time)
-        df = df.rename(columns={"TIME(hh:mm:ss)": "TIME(DAYS)"})
-
-        ## make a new column: pressure/temp
-        # print(df.columns)
-        # df['PSI/degC'] = df["PRESSURE(PSI)"]/df["ROOM TEMPERATURE(C)"]
-
-        return df
 
 
 ################################################################################
