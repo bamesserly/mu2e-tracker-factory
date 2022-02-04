85d1b86d715a332dc0b94c07e608d14068c3ffaa
Ben Messerly <bamesserly@gmail.com>
2022-02-04 17:15:02 -0600

    Better filename parsing. Simpler autoloader.

--- guis/panel/leak/leak_autoloader.py
+++ guis/panel/leak/leak_autoloader.py
@@ -1,26 +1,25 @@
-import guis.panel.leak.load_leak_csv_into_db as load, os, shutil
+# Purpose of this script is to call the db-loading function on all leak rate
+# files.
+# Mostly writing it to run it once on all the old data that never got loaded in
+# the first place.
+import guis.panel.leak.load_leak_csv_into_db as load
+import os, shutil
 from pathlib import Path
+from guis.common.getresources import GetProjectPaths
 
-    # function to copy files to problem_files folder
-def copy_file(filename, filepath):
-    rootdir = "data/Panel data/FinalQC/Leak/RawData/"
-    copy_path = "guis/panel/leak/problem_files/"
-    original = filepath
-    target = copy_path + filename
-    print(target)
-    shutil.copyfile(original, target)
+from guis.common.panguilogger import SetupPANGUILogger
 
-
+logger = SetupPANGUILogger("root", "load_all_leak")
 
 if __name__ == "__main__":
-    rootdir = "data/Panel data/FinalQC/Leak/RawData/"
-    
+    rootdir = GetProjectPaths()["panelleakdata"]
+
     # iterate through files in directory and subdirectories, attempting to save them to the DATABASE
     # otherwise, copy them to the problem_files folder
-    for root, dirs, files in os.walk(rootdir):
-        for filename in files:
-            try:
-                filepath = Path(os.path.join(root, filename)).absolute()
-                load.main(filepath)
-            except:
-                copy_file(filename, filepath)
+    for f in rootdir.rglob("*.txt"):
+        try:
+            load.main(f)
+        except KeyboardInterrupt:
+            logger.error(f"load interrupted for {f}")
+        except Exception as e:
+            logger.error(f"load failed for {f}, {e}")
