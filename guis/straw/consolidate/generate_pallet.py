e973d3b095da664256ba0452b88c7a2c5033a3ff
Ben Messerly <bamesserly@gmail.com>
2022-07-01 16:16:45 -0500

    Initial commit to merge generate pallet and consolidate.

--- guis/straw/consolidate/generate_pallet.py
+++ guis/straw/consolidate/generate_pallet.py
@@ -18,33 +18,17 @@
 from datetime import datetime
 from guis.common.getresources import GetProjectPaths
 from guis.common.panguilogger import SetupPANGUILogger
-from guis.straw.consolidate.consolidate_utils import finalizeStraws
+from guis.straw.consolidate.consolidate_utils import (
+    finalizeStraws,
+    save_to_csv,
+    kGENERATE_STEPS,
+    save_to_db,
+)
 from guis.common.db_classes.straw import Straw
 from guis.common.db_classes.straw_location import StrawLocation, CuttingPallet
 from guis.common.merger import isolated_automerge
 
 import logging
-
-def save_to_csv(pfile, is_new_cpal, straw_pass_list, workers):
-    logger = logging.getLogger("root")
-    now = datetime.now()
-    date = now.strftime("%Y-%m-%d_%H:%M")
-    header = "Time Stamp, Task, 24 Straw Names/Statuses, Workers, ***24 straws initially on retest pallet***\n"
-    straws_passed_str = ",P,".join(straw_pass_list) + ",P,"
-    logger.debug(straws_passed_str)
-    with open(pfile, "a+") as myfile:
-        if is_new_cpal:
-            myfile.write(header)
-        else:
-            # navigate to last character in file
-            myfile.seek(myfile.tell() - 1)
-            # make sure it's a newline
-            if myfile.read() != "\n":
-                myfile.write("\n")
-        myfile.write(date + ",prep," + straws_passed_str + workers + "\n")
-        myfile.write(date + ",ohms," + straws_passed_str + workers + "\n")
-        myfile.write(date + ",C-O2," + straws_passed_str + workers + "\n")
-
 
 def run():
     pallet_dir = GetProjectPaths()["pallets"]
@@ -64,7 +48,7 @@ def run():
         f"worker:{workers}, cpalid:{cpal_id}, cpal:{cpal_num}, is_new_cpal:{is_new_cpal}"
     )
 
-    straws_passed_list = []
+    straws_passed = []
     for i in range(24):
         while True:
             straw = input("Scan barcode #" + str(i + 1) + " ")
@@ -76,7 +60,7 @@ def run():
                 print("Invalid straw entered. Try again.")
                 continue
 
-            if straw in straws_passed_list and straw != "_______":
+            if straw in straws_passed and straw != "_______":
                 print("***********************************************")
                 print("WARNING DUPLICATE STRAW NUMBER ENTERED.")
                 print("DUPLICATE STRAW HAS NOT BEEN SAVED.")
@@ -86,15 +70,15 @@ def run():
                 print("***********************************************")
                 continue
             else:
-                straws_passed_list.append(straw.upper())
+                straws_passed.append(straw.upper())
                 break
 
-    straws_passed_list = finalizeStraws(
-        straws_passed=straws_passed_list, worker=None, require_leak_pass=False
+    straws_passed = finalizeStraws(
+        straws_passed=straws_passed, worker=None, require_leak_pass=False
     )
 
-    save_to_csv(pfile, is_new_cpal, straws_passed_list, workers)
-    CuttingPallet.save_to_db(straws_passed_list, cpal_id, cpal_num)
+    save_to_csv(pfile, is_new_cpal, straws_passed, workers, kGENERATE_STEPS)
+    save_to_db(straws_passed, cpal_id, cpal_num)
 
     isolated_automerge()
 
