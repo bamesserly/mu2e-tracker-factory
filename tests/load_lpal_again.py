4184ffc7c36ae2f80ac98f55408a6a0ec2d040cb
adamarnett <adam.arnett1@gmail.com>
2022-11-01 18:46:11 -0500

    Added more error checking to the script. - see title

--- tests/load_lpal_again.py
+++ tests/load_lpal_again.py
@@ -54,8 +54,10 @@ def newEntry(strawID, position, present, connection, time_in=None, time_out=None
     try:
         sendIt = connection.execute(query)
     except sqla.exc.OperationalError as e:
+        logger.error(e)
         return e
     except sqla.exc.IntegrityError as e:
+        logger.error(e)
         return e
 
     return "Success"
@@ -65,6 +67,81 @@ def findSpecial(search, find):
         return 0
     else:
         return str(search).find(find)
+
+
+# check if any straw present entries for straw contradict eachother
+#   i.e. check for timestamps that indicate it was in two places at once,
+#   or if it supposedly exists in two places at once right now
+# ret val | meaning
+#       0 | no contradictions, all is good
+#       1 | multiple of the same position
+#       2 | marked as present in multiple places
+#       3 | timestamp conflict
+def checkStrawIntegrity(straw):
+    # dict of positions
+    positions = {}
+    # bool for tracking number of "present = 1" found
+    presentFound = False
+
+    # loop through entries
+    for thing in StrawPresent.query().filter(StrawPresent.straw == straw.id).all():
+        # if key (position) exists in dict already --> two positions found
+        if thing.position in positions:
+            logger.error(f'Two of the same position detected for straw {straw}')
+            return 1
+
+        # if straw is present at this position
+        if thing.present == 1:
+            # if it hasn't been found elsewhere yet
+            if not presentFound:
+                # mark that we found it
+                presentFound = True
+            # if it has been found elsewhere
+            else:
+                # it exists in two places which is bad
+                logger.error(f'Multiple present = 1 for straw {straw}')
+                return 2
+        
+        # CPALs don't usually have out times so lets not include those
+        cpals = StrawPresent.query().filter(StrawPresent.straw == straw.id).join(StrawPosition, StrawPosition.id == StrawPresent.position).join(StrawLocation, StrawLocation.id == StrawPosition.location).filter(StrawLocation.location_type == "CPAL").all()
+        if len(cpals) == 0:
+            # add to dict
+            #   {position : (present, time_in, time_out)}
+            positions[thing.position] = (thing.present, thing.time_in, thing.time_out)
+    # end of loop body
+
+    # check timestamps for weirdness
+    # there is likely a faster way to do this, n^2 time is heresy
+    # for each position
+    for key1 in positions:
+        # get the timestamps (tr abbreviation = time range)
+        tr1 = (
+            positions[key1][1] if positions[key1][1] is not None else -1,
+            positions[key1][2] if positions[key1][2] is not None else 1e12
+        )
+        # for each other key
+        for key2 in positions:
+            # each OTHER key
+            if key1 == key2:
+                # skip if it's the same one
+                continue
+            # get the timestamps for another position
+            tr2 = (
+                positions[key2][1] if positions[key2][1] is not None else -1,
+                positions[key2][2] if positions[key2][2] is not None else 1e12
+            )
+            # if tr2 start or end is within range of tr1 we have a problem
+            if tr2[0] > tr1[0] and tr2[0] < tr1[1]:
+                logger.error(f'Timestamp conflict for straw {straw}')
+                return 3
+            if tr2[1] > tr1[0] and tr2[1] < tr1[1]:
+                logger.error(f'Timestamp conflict for straw {straw}')
+                return 3
+
+
+
+    # didn't find any red flags
+    return 0
 
 
 ###################################################################################################
@@ -88,6 +165,11 @@ def run(lpal_file):
 
     # get LPAL location ID
     lpalLoc = StrawLocation.query().filter(StrawLocation.location_type == 'LPAL').filter(StrawLocation.number == int((sys.argv[1])[4:8])).one_or_none()
+    # if no LPAL found
+    if lpalLoc == None:
+        logger.error(f'LPAL {(sys.argv[1])[4:8]} not found, aborting.')
+        # bad ending :(
+        return 1
 
     # for each straw (row) from the file...
     for row in rows:
@@ -95,9 +177,22 @@ def run(lpal_file):
         straw = get_or_create_straw((row["Straw"])[2:])
 
         # get the id of the correct position
-        position = StrawPosition.query().filter(StrawPosition.location == lpalLoc.id).filter(StrawPosition.position_number == row["Position"]).one_or_none()
+        position = (StrawPosition.query().filter(StrawPosition.location == lpalLoc.id).filter(StrawPosition.position_number == row["Position"]).one_or_none()).id
+
+        # search for any location that is an LPAL, if found we can skip this one
+        inLPAL = any(findSpecial(e, "LPAL") for e in straw.locate(current=False))
+        if inLPAL:
+            logger.info(f'Aborting submission for straw {straw.id}, LPAL entry already present.')
+            # verify data accuracy here... ?
+            continue # possibly heresy
+
+        integrity = checkStrawIntegrity(straw)
+        if integrity != 0:
+            # problem found!
+            logger.info(f'Aborting submission for straw {straw.id} due to integrity error.')
+            continue
         
-        # search for any location that is a panel
+        # search for any location that is a panel (to get an out time for the LPAL)
         inPanel = any(findSpecial(e, "MN") for e in straw.locate())
         # if there's a panel entry in straw_present for this straw get the time it entered the panel
         if inPanel:
@@ -105,22 +200,23 @@ def run(lpal_file):
             panelInTime = panelEntry.time_in
 
 
-        # also search for presence in an LPAL
-        # if this is true, there's already an LPAL entry in straw_present for this
-        #   straw and it could be skipped (although we could verify the accuracy...)
-        inLPAL = any(findSpecial(e, "LPAL") for e in straw.locate())
-        if inLPAL:
-            # verify data here... ?
-            continue # possibly heresy
-        else:
+
+        if not inLPAL:
             logger.info(f"Submitting straw ST{straw.id} to DB with values:")
-            logger.info(f"Straw: {straw.id}, Position: {lpalLoc}, Present: {not inPanel}, time_in: {row['Timestamp']}, time_out: {panelInTime if inPanel else None}")
+            logger.info(f"Straw: {straw.id}, Position: {position}, Present: {not inPanel}, time_in: {row['Timestamp']}, time_out: {panelInTime if inPanel else None}")
             newEntry(straw.id, position, not inPanel, connection, row['Timestamp'], (panelInTime if inPanel else None))
             #StrawPresent(straw.id, position, not inPanel, row['Timestamp'], (panelInTime if inPanel else None))
-            
+
+        integrity = checkStrawIntegrity(straw)
+        if integrity != 0:
+            # problem found!
+            logger.info(f'The script may have corrupted the data for straw {straw.id}.  Sowwy! >_<')
+
+    # end of for row in rows loop
+    # !!! Loop contains two continue statements, one in the "if integrity != 0", and one in the "if inLPAL" block.
 
 
-
+    # end of script
     return 0
 
 if __name__ == "__main__":
